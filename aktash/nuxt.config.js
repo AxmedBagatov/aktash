1e2335b79be26da56f034377547e8d57961940a7
AxmedBagatov <bagatov2002@mail.ru>
2024-11-22 22:35:01 +0300

    Автоматический коммит изменений

--- aktash/nuxt.config.js
+++ aktash/nuxt.config.js
@@ -6,6 +6,14 @@ export default {
     port: 80,
     host: '0.0.0.0',
   },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '/shop',
+        component: resolve(__dirname, 'pages/shop/shop.vue')
+      });
+    }
+  },
   head: {
     titleTemplate: '%s - aktash',
     title: 'aktash',
