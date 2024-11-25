50005aeb5b134e0fbe5557e747e559651d767f0c
AxmedBagatov <bagatov2002@mail.ru>
2024-11-25 11:35:02 +0300

    Автоматический коммит изменений

--- aktash/store/index.js
+++ aktash/store/index.js
@@ -20,66 +20,66 @@ export const mutations = {
   }
 };
 
-export const actions = {
-  async fetchProducts({ commit }) {
-    try {
-      commit('setLoading', true);
-      const response = await fetch('http://192.168.62.129:4000/api/products');
-      if (response.ok) {
-        const data = await response.json();
-        commit('setProducts', data);
-      } else {
-        commit('setErrorMessage', 'Ошибка при получении данных продуктов');
-        console.error('Ошибка при получении данных продуктов');
+  export const actions = {
+    async fetchProducts({ commit }) {
+      try {
+        commit('setLoading', true);
+        const response = await fetch('http://192.168.62.129:4000/api/products');
+        if (response.ok) {
+          const data = await response.json();
+          commit('setProducts', data);
+        } else {
+          commit('setErrorMessage', 'Ошибка при получении данных продуктов');
+          console.error('Ошибка при получении данных продуктов');
+        }
+      } catch (error) {
+        commit('setErrorMessage', 'Ошибка сети: ' + error.message);
+        console.error('Ошибка сети:', error);
+      } finally {
+        commit('setLoading', false);
       }
-    } catch (error) {
-      commit('setErrorMessage', 'Ошибка сети: ' + error.message);
-      console.error('Ошибка сети:', error);
-    } finally {
-      commit('setLoading', false);
-    }
-  },
+    },
 
-  async fetchCategories({ commit }) {
-    try {
-      commit('setLoading', true);
-      const response = await fetch('http://192.168.62.129:4000/api/categories');
-      if (response.ok) {
-        const data = await response.json();
-        commit('setCategories', data);
-      } else {
-        commit('setErrorMessage', 'Ошибка при получении данных категорий');
-        console.error('Ошибка при получении данных категорий');
+    async fetchCategories({ commit }) {
+      try {
+        commit('setLoading', true);
+        const response = await fetch('http://192.168.62.129:4000/api/categories');
+        if (response.ok) {
+          const data = await response.json();
+          commit('setCategories', data);
+        } else {
+          commit('setErrorMessage', 'Ошибка при получении данных категорий');
+          console.error('Ошибка при получении данных категорий');
+        }
+      } catch (error) {
+        commit('setErrorMessage', 'Ошибка сети: ' + error.message);
+        console.error('Ошибка сети:', error);
+      } finally {
+        commit('setLoading', false);
       }
-    } catch (error) {
-      commit('setErrorMessage', 'Ошибка сети: ' + error.message);
-      console.error('Ошибка сети:', error);
-    } finally {
-      commit('setLoading', false);
-    }
-  },
+    },
 
-  async fetchProductsByCategory({ commit }, categoryId) {
-    try {
-      commit('setLoading', true);
-      const response = await fetch(`http://192.168.62.129:4000/api/products?category_id=${categoryId}`);
-      
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);  // Проверка данных
-        commit('setProducts', data);  // Сохраняем данные в хранилище
-      } else {
-        commit('setErrorMessage', 'Ошибка при загрузке продуктов для категории');
-        console.error('Ошибка при загрузке продуктов для категории');
+    async fetchProductsByCategory({ commit }, categoryId) {
+      try {
+        commit('setLoading', true);
+        const response = await fetch(`http://192.168.62.129:4000/api/products?category_id=${categoryId}`);
+        
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);  // Проверка данных
+          commit('setProducts', data);  // Сохраняем данные в хранилище
+        } else {
+          commit('setErrorMessage', 'Ошибка при загрузке продуктов для категории');
+          console.error('Ошибка при загрузке продуктов для категории');
+        }
+      } catch (error) {
+        commit('setErrorMessage', 'Ошибка сети: ' + error.message);
+        console.error('Ошибка сети:', error);
+      } finally {
+        commit('setLoading', false);  // Останавливаем загрузку
       }
-    } catch (error) {
-      commit('setErrorMessage', 'Ошибка сети: ' + error.message);
-      console.error('Ошибка сети:', error);
-    } finally {
-      commit('setLoading', false);  // Останавливаем загрузку
     }
-  }
-};
+  };
 
 export const getters = {
   getProducts(state) {
