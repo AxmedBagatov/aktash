61751152730c4d75f62d6a8483bee62eac48722b
AxmedBagatov <bagatov2002@mail.ru>
2024-11-25 12:00:01 +0300

    Автоматический коммит изменений

--- aktash/layouts/default.vue
+++ aktash/layouts/default.vue
@@ -4,16 +4,17 @@
       <main>
         <Nuxt />
       </main>
-      
+      <Footer />
     </div>
   </template>
   
   <script>
   import Header from '~/components/Header.vue';
-  
+  import Footer from '~/components/Footer.vue';
   export default {
     components: {
       Header,
+      Footer,
     },
   };
   </script>
